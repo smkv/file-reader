1322096d3d8da53022c075a3b6f0b65124b4eb64
smkv <andrei.samkov@gmail.com>
2015-12-05 14:35:28 +0200

    log4j

--- src/ee/smkv/server/Server.java
+++ src/ee/smkv/server/Server.java
@@ -1,17 +1,22 @@
 package ee.smkv.server;
 
+import org.apache.log4j.Logger;
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import java.io.IOException;
 
 public class Server {
 
-  public static void main(String[] args) throws IOException, InterruptedException {
-    String path = args.length > 0 ? args[0] : ".";
-    HttpServer httpServer = HttpServer.createSimpleServer("/", Integer.valueOf(System.getProperty("port", "8080")));
-    httpServer.getServerConfiguration().addHttpHandler(new FileListHttpHandler(path), "/files");
-    httpServer.getServerConfiguration().addHttpHandler(new ResourceHttpHandler(), "/resources");
-    httpServer.start();
-    Thread.currentThread().join();
-  }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String path = args.length > 0 ? args[0] : ".";
+        Integer port = Integer.valueOf(System.getProperty("port", "8080"));
+
+        Logger log = Logger.getLogger(Server.class);
+        log.info("Starting http server on port " + port + " to directory" + path);
+        HttpServer httpServer = HttpServer.createSimpleServer("/", port);
+        httpServer.getServerConfiguration().addHttpHandler(new FileListHttpHandler(path), "/files");
+        httpServer.getServerConfiguration().addHttpHandler(new ResourceHttpHandler(), "/resources");
+        httpServer.start();
+        Thread.currentThread().join();
+    }
 }
